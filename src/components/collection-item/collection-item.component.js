361392170dc7b75fe927b23d6b944944875d3412
Chintan Gandhi <cgandhi4@gmail.com>
2020-08-18 19:34:37 +0530

    Add to cart button added & styled collection item

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.scss'
 
 const CollectionItem = ({name, price, imageUrl}) => {
@@ -13,7 +15,8 @@ const CollectionItem = ({name, price, imageUrl}) => {
         <div className='collection-footer'>
            <span className='name'>{name}</span>
            <span className='price'>{price}</span>
-        </div>       
+        </div> 
+        <CustomButton inverted> Add to cart </CustomButton>     
      </div>
    ); 
 }
