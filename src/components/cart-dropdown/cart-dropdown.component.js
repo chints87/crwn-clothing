df2d682438a4b12e380a80af480f46d7c9595a2f
Chintan Gandhi <cgandhi4@gmail.com>
2020-08-19 20:52:12 +0530

    Add disptach to CheckOut dropdown button

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -6,9 +6,10 @@ import { withRouter } from 'react-router'
 import CustomButtom from '../custom-button/custom-button.component';
 import CardItem from '../cart-item/cart-item.component';
 import {selectCartItems} from '../../redux/cart/cart-selector';
+import {toggleCartHidden} from '../../redux/cart/cart-action';
 import './cart-dropdown.scss'
 
-const CartDropDown = ({ cartItems, history }) =>(
+const CartDropDown = ({ cartItems, history, dispatch }) =>(
     <div className="cart-dropdown">
        <div className="cart-items">{
            cartItems.length? (
@@ -16,7 +17,10 @@ const CartDropDown = ({ cartItems, history }) =>(
             <CardItem key={cartItem.id} item={cartItem}/>)))
             : (<span className="empty-message">Your cart is empty</span>)
         }</div>          
-       <CustomButtom onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButtom>
+       <CustomButtom onClick={() => {
+           history.push('/checkout')
+           dispatch(toggleCartHidden())
+       }}>GO TO CHECKOUT</CustomButtom>
     </div>
 
 )
