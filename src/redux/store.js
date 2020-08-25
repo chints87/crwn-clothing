0596c4439466e2d3980621dd1f4a61ce562b7e7e
Chintan Gandhi <cgandhi4@gmail.com>
2020-08-25 17:18:44 +0530

    Corrected store.js for NODE_ENV

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import rootReducer from './root-reducer'
 
 const middlewares = [logger]
 
-if(process.env.NODE.ENV === 'development'){
+if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger)
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
